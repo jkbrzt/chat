06c181bd1359ed7447730ad82a6874b44b79be8b
Jakub Roztočil <jakub@roztocil.name>
2012-02-08 23:20:08 +0100

    initial

--- app.py
+++ app.py
@@ -4,33 +4,6 @@ import flask
 import redis
 
 
-HOME_TEMPLATE = """
-<!doctype html>
-<title>chat</title>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<style>body { max-width: 500px; margin: auto; padding: 1em; background: black; color: #fff; font: 16px/1.6 menlo, monospace; }</style>
-<p><b>hi, %s!</b></p>
-<p>Message: <input id="in" /></p>
-<pre id="out"></pre>
-<script>
-    function sse() {
-        var source = new EventSource('/stream');
-        var out = document.getElementById('out');
-        source.onmessage = function(e) {
-            out.innerHTML =  e.data + '\\n' + out.innerHTML;
-        };
-    }
-    $('#in').keyup(function(e){
-        if (e.keyCode == 13) {
-            $.post('/post', {'message': $(this).val()});
-            $(this).val('');
-        }
-    });
-    sse();
-</script>
-
-"""
-
 app = flask.Flask(__name__)
 app.secret_key = 'asdf'
 red = redis.StrictRedis()
@@ -44,21 +17,13 @@ def event_stream():
         yield 'data: %s\n\n' % message['data']
 
 
-@app.route('/')
-def home():
-    if 'user' not in flask.session:
-        return flask.redirect('/login')
-    return HOME_TEMPLATE % flask.session['user']
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'POST':
         flask.session['user'] = flask.request.form['user']
         return flask.redirect('/')
-    return """
-    <form action="" method="post">user: <input name="user">
-    """
+    return '<form action="" method="post">user: <input name="user">'
+
 
 @app.route('/post', methods=['POST'])
 def post():
@@ -66,13 +31,44 @@ def post():
     user = flask.session.get('user', 'anonymous')
     now = datetime.datetime.now().replace(microsecond=0).time()
     red.publish('chat', u'[%s] %s: %s' % (now.isoformat(), user, message))
-    return ''
 
 
 @app.route('/stream')
 def stream():
     return flask.Response(event_stream(),
                           mimetype="text/event-stream")
+
+
+@app.route('/')
+def home():
+    if 'user' not in flask.session:
+        return flask.redirect('/login')
+    return """
+    <!doctype html>
+    <title>chat</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <style>body { max-width: 500px; margin: auto; padding: 1em; background: black; color: #fff; font: 16px/1.6 menlo, monospace; }</style>
+    <p><b>hi, %s!</b></p>
+    <p>Message: <input id="in" /></p>
+    <pre id="out"></pre>
+    <script>
+        function sse() {
+            var source = new EventSource('/stream');
+            var out = document.getElementById('out');
+            source.onmessage = function(e) {
+                out.innerHTML =  e.data + '\\n' + out.innerHTML;
+            };
+        }
+        $('#in').keyup(function(e){
+            if (e.keyCode == 13) {
+                $.post('/post', {'message': $(this).val()});
+                $(this).val('');
+            }
+        });
+        sse();
+    </script>
+
+    """ % flask.session['user']
 
 
 if __name__ == '__main__':
